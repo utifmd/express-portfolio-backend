5946fa770ecc9007772be60b422619a3c5d1eaf3
utifmd <utifmd@gmail.com>
2023-07-12 20:43:51 +0700

    try to publish public dir #3

--- app.ts
+++ app.ts
@@ -14,5 +14,5 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(express.static("public"));
+app.use("/", express.static("public"));
 app.use(router);
